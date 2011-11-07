b30264b21419d7984cf3d0cbd3c5d5a7ccf63f8a
huangyingw@linux_programming <huangyingw@gmail.com>
2011-11-07 22:22:30 +0800

    n

--- delete_sub_str.cc
+++ delete_sub_str.cc
@@ -1,30 +1,44 @@
 #include"stdio.h"
 #include"stdlib.h"
 #include<iostream>
+int delete_sub_str(const char *str, const char *sub_str, char *result_str)  
+{  
+  int count = 0;  
+  int k = 0;  
+  char *p3 = result_str;  
+  for(int i = 0; str[i] != '\0'; i++)  
+  {  
+    int tem = i; //tem保留主串中的起始判断下标位置   
+    int j = 0;  
+    while((str[i] != '\0') && (sub_str[j] != '\0') && (str[i] == sub_str[j]))  
+    {  
+      i++;  
+      j++;  
+    }  
+    if(sub_str[j] != '\0')  
+    {  
+      i = tem;  
+      p3[k] = str[i];   
+      k++;  
+    }  
+    else  
+    {  
+      count += 1;  
+      i--;  
+    }         
+  }  
+  return count;  
+}  
 
-using namespace std;
+int main()  
+{  
+  char *str = "12fuck345fuck678fuck9";  
+  char *sub = "fuck";  
+  char res[50] ="";  
+  int count = delete_sub_str(str, sub, res);  
+  printf("子字符串的个数是：%d\n", count);  
+  printf("删除子字符串后：\n%s\n", res);  
+  return 0;  
+}  
 
-//s=a+aa+aaa+aaaa+aa...a
-int num_of_a(int a,int num)
-{
-  int current_a=0,sum=0;
 
-  for (int i=1;i<= num;i++)
-  {
-    current_a=0;
-    for (int j=0;j<i;j++)
-    {
-      current_a*=10;
-      current_a+=a;
-    }
-    sum+=current_a;
-  }
-  return sum;
-}
-
-int main() 
-{
-  cout<<num_of_a(3,5)<<endl;//37035
-  cout<<num_of_a(4,5)<<endl;//49380
-  return 0;
-}
