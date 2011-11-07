8e685edad45258d8baba4d279e8c32f50662507f
huangyingw@linux_programming <huangyingw@gmail.com>
2011-11-07 23:00:10 +0800

    n

--- delete_sub_str.cc
+++ delete_sub_str.cc
@@ -6,36 +6,29 @@ int delete_sub_str(const char *str, const char *sub_str, char *result_str)
 {  
   int count=0;
   int i=0,j=0,temp=0;
+  char * origin_result=result_str;
   while ( str[i]!=sub_str[j]) i++;
-  temp=i;
-  while ( str[i++]==sub_str[j++] && '\0' != sub_str[j]); 
-  if ( '\0' == str[i])
+  while ( '\0' != str[i])
   {
+    temp=i;
+    while ( str[i++]==sub_str[j++] && '\0' != sub_str[j]); 
     for ( int k=0;k<temp;k++)
     {
       *result_str++=str[k];
+      cout<<str[k]<<endl;
     }
-  }
-  else
-  {
-    for ( int k=0;k<temp;k++)
-    {
-      *result_str++=str[k];
-    }
+    //cout<<origin_result<<endl;
   }
   return count;  
 }  
 
 int main()  
 {  
-  char *str = "12fuck345fuck678fuck9";  
-  //char *str = "12fuck";  
+  //char *str = "12fuck345fuck678fuck9";  
+  char *str = "12fuck";  
   char *sub = "fuck";  
   char res[50] ="";  
   int count = delete_sub_str(str, sub, res);  
-  printf("子字符串的个数是：%d\n", count);  
-  printf("删除子字符串后：\n%s\n", res);  
+  cout<<res<<endl;
   return 0;  
 }  
-
-
