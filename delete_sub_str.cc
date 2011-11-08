a449c4e9985a87329ab333b4912cf477427e49b9
huangyingw@linux_programming <huangyingw@gmail.com>
2011-11-08 10:38:12 +0800

    n

--- delete_sub_str.cc
+++ delete_sub_str.cc
@@ -1,3 +1,4 @@
+// delete_sub_str.cpp : Defines the entry point for the console application.
 #include"stdio.h"
 #include"stdlib.h"
 #include<iostream>
@@ -8,28 +9,46 @@ int delete_sub_str(const char *str, const char *sub_str, char *result_str)
   int i=0,j=0,temp=0;
   char * origin_result=result_str;
   while ( str[i]!=sub_str[j]) i++;
+  for ( int k=0;k<i;k++)
+  {
+    *result_str++=str[k];
+  }
+
   while ( '\0' != str[i])
   {
     temp=i;
     while ( str[i++]==sub_str[j++] && '\0' != sub_str[j]); 
-    for ( int k=0;k<temp;k++)
-    {
-      *result_str++=str[k];
-      cout<<k<<","<<str[k]<<endl;
-    }
-    cout<<"result->"<<origin_result<<endl;
+	j=0;
+	while ( str[i] != sub_str[j] && '\0' != str[i]) 
+	{
+		*result_str++=str[i];
+		i++;
+	}
+    
   }
+  *result_str='\0';
   return count;  
 }  
 
-int main()  
-{  
-  char *str = "12fuck345";  
-  //char *str = "12fuck345fuck678fuck9";  
-  //char *str = "12fuck";  
+int main()
+{
+  char *  str = "";  
   char *sub = "fuck";  
   char res[50] ="";  
-  int count = delete_sub_str(str, sub, res);  
+
+  str = "12fuck345";  
+  delete_sub_str(str, sub, res);  
   cout<<res<<endl;
+
+  str = "12fuck";  
+  delete_sub_str(str, sub, res);  
+  cout<<res<<endl;
+
+
+  str = "12fuck345fuck678fuck9";  
+  delete_sub_str(str, sub, res);  
+  cout<<res<<endl;
+
   return 0;  
-}  
+}
+
