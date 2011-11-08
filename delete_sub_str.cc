865b999fe5be52c8e89116dd6429285790fd929b
huangyingw@linux_programming <huangyingw@gmail.com>
2011-11-08 19:48:46 +0800

    n

--- delete_sub_str.cc
+++ delete_sub_str.cc
@@ -3,43 +3,100 @@
 #include"stdlib.h"
 #include<iostream>
 using namespace std;
-int delete_sub_str(const char *str, const char *sub_str, char *result_str) {  
+int delete_sub_str(const char *str, const char *sub_str, char *result_str)  
+{  
   int count=0;
-  int i=0,j=0;
+  int i=0,j=0,temp=0;
+  char * origin_result=result_str;
   while ( str[i]!=sub_str[j]) i++;
-  for ( int k=0;k<i;k++) {
+  for ( int k=0;k<i;k++)
+  {
     *result_str++=str[k];
   }
-  while ( '\0' != str[i]) {
-    while ( str[i++]==sub_str[j++] && '\0' != sub_str[j]); 
-    j=0;
-    while ( str[i] != sub_str[j] && '\0' != str[i]) {
-      *result_str++=str[i];
-      i++;
-    }
 
+  while ( '\0' != str[i])
+  {
+    temp=i;
+    while ( str[i++]==sub_str[j++] && '\0' != sub_str[j]); 
+	j=0;
+	while ( str[i] != sub_str[j] && '\0' != str[i]) 
+	{
+		*result_str++=str[i];
+		i++;
+	}
+    
   }
   *result_str='\0';
   return count;  
 }  
 
-int main() {
+int delete_sub_str(char str[], const char *sub_str)  
+{  
+	int count=0;
+	bool found=true;
+	int i=0,j=0,temp=0,p=0,q=0,nav;
+	char * origin_str=str;
+	int len=sizeof(sub_str)/sizeof(char);
+	while(found)
+	{
+		i=0;
+		j=0;
+		found=false;
+		while ( str[i]!=sub_str[j]) i++;
+		p=i;
+		
+		while (sub_str[j]!='\0' && str[i++]==sub_str[j++]);
+		if (sub_str[j] == '\0')
+		{
+			found=true;
+		}
+		else
+		{
+			return 0;
+		}
+			
+		nav=q=i;
+		while (str[nav])
+		{
+			str[p++]=str[nav++];
+		}
+		if (str[nav]=='\0')
+			str[p]='\0';
+	}
+	
+	return count;  
+}  
+
+int main()
+{
   char *  str = "";  
   char *sub = "fuck";  
   char res[50] ="";  
 
-  str = "12fuck345";  
-  delete_sub_str(str, sub, res);  
-  cout<<res<<endl;
+  //str = "12fuck345";  
+  //delete_sub_str(str, sub, res);  
+  //cout<<res<<endl;
 
-  str = "12fuck";  
-  delete_sub_str(str, sub, res);  
-  cout<<res<<endl;
+  //str = "12fuck";  
+  //delete_sub_str(str, sub, res);  
+  //cout<<res<<endl;
 
-  str = "12fuck345fuck678fuck9";  
-  delete_sub_str(str, sub, res);  
-  cout<<res<<endl;
+
+  //str = "12fuck345fuck678fuck9";  
+  //delete_sub_str(str, sub, res);  
+  //cout<<res<<endl;
+
+  //char str1[] = "12fuck";  
+  //delete_sub_str(str1, sub);  
+  //cout<<str1<<endl;
+
+  //char str2[] = "12fuck345";  
+  //delete_sub_str(str2, sub);  
+  //cout<<str2<<endl;
+
+  char str3[] = "12fuck345fuck678fuck9";
+  delete_sub_str(str3, sub);  
+  cout<<str3<<endl;
 
   return 0;  
 }
-
