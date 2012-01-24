ed87c870c51e558f0100ea6d9a164580372428a6
huangyingw@linux_programming <huangyingw@gmail.com>
2012-01-24 21:28:19 +0800

    n

--- src/delete_sub_str.java
+++ src/delete_sub_str.java
@@ -2,44 +2,55 @@ public class delete_sub_str {
 	static char[] delete_sub_str_method(char[] str, char[] sub_str) {
 		char[] result_str = new char[str.length - sub_str.length];
 		int count = 0;
-		int i = 0, j = 0;
-		int index = 0;
-		while (str[i] != sub_str[j])
-			i++;
-		for (int k = 0; k < i; k++) {
-			result_str[index++] = str[k];
-		}
+		int i = 0;
+		int j = 0;
+		int index = 0, temp = 0;
 
 		while (i < str.length) {
-			while (i < str.length && j < sub_str.length
-					&& str[i++] == sub_str[j++])
-				;
-			if (i < str.length)
-				count++;
+			temp = i;
 			j = 0;
-			while (i < str.length && str[i] != sub_str[j]) {
-				result_str[index++] = str[i++];
+			while (temp < str.length && j < sub_str.length
+					&& str[temp++] == sub_str[j++])
+				;
+			if (j == sub_str.length) {
+				count++;
+				i = temp;
+			} else {
+				while (i < temp) {
+					result_str[index++] = str[i++];
+				}
 			}
+			j = 0;
+			temp = i;
+			while (temp < str.length && str[temp] != sub_str[j])
+				temp++;
+			while (i < temp)
+				result_str[index++] = str[i++];
 		}
+
 		return result_str;
 	}
 
 	static int count_sub(char[] source, char[] sub) {
-		int result = 0;
-		int i = 0, j = 0;
+		int count = 0;
+		int i = 0;
+		int j = 0;
+		int index = 0, temp = 0;
+
 		while (i < source.length) {
+			temp = i;
 			j = 0;
-			while (source[i++] != sub[j])
+			while (temp < source.length && j < sub.length
+					&& source[temp++] == sub[j++])
 				;
-			i--;
-			while (i < source.length && j < sub.length
-					&& source[i++] == sub[j++])
-				;
-			if (j < sub.length) {
-				result++;
+			if (j == sub.length) {
+				count++;
+				i = temp;
 			}
+			i++;
 		}
-		return result;
+
+		return count;
 	}
 
 	public static void main(String arg[]) {
@@ -64,8 +75,14 @@ public class delete_sub_str {
 		System.out.println(delete_sub_str_method(
 				"12fuck345fuck678fuck9fuc".toCharArray(), sub));
 
+		System.out.println(delete_sub_str_method(
+				"12fck345fuck678fuck9fuc".toCharArray(), sub));
+
 		System.out.println(count_sub("12fuck345fuck678fuck9fuc".toCharArray(),
 				"fuck".toCharArray()));
+		System.out.println(count_sub("12fuk345fuck678fuck9fuc".toCharArray(),
+				"fuck".toCharArray()));
+
 		/*
 		 * 12345 12 3,123456789 12 12345 3,123456789 3,123456789fuc 3
 		 */
